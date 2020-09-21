17501dc93bf42b898c370b9b8102a9803f9cb308
Firdavs Abdulloev <67782167+firdavs-projects@users.noreply.github.com>
2020-09-21 09:06:36 +0500

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,8 +2,8 @@
     <div>
         <h2>Home page</h2>
         <p>
-            For more information
-            firdavs.abdulloev@gmail.com
+            For more information:
+            firdavs.abdulloev.7725@gmail.com
         </p>
         <router-link to="/todos">Todos</router-link>
     </div>
